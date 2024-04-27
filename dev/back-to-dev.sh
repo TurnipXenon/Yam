3473ffde957111a173a533ceb896d2caa3b39642
turnipxenon <turnipxenon@gmail.com>
2024-04-26 19:22:31 -0600

    Add bash script back to dev

---
+++ dev/back-to-dev.sh
@@ -0,0 +1,2 @@
+﻿# This scripts allows us to quickly build on top of the latest branch with our dev branch
+git checkout main && git pull origin main && git branch -d turnip/dev && git checkout -b turnip/dev
