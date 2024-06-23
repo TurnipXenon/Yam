82493f3d55172671b0f766df97ff18d8183831e7
turnipxenon <turnipxenon@gmail.com>
2024-06-23 01:27:12 -0600

    Update PooledBeat.Tick test to consider reusing itself

--- Yam.Core.Test/Rhythm/Services/BeatPooler/PooledBeatTest.cs
+++ Yam.Core.Test/Rhythm/Services/BeatPooler/PooledBeatTest.cs
@@ -36,40 +36,51 @@ public class PooledBeatTest
 			var pooler = new Mock<Core.Rhythm.Servers.BeatPooler>(resource.Object);
 			pooledBeat.Initialize(pooler.Object, resource.Object);
 
-			pooledBeat.Tick();
-			host.Verify(h => h.Deactivate(), Times.Once);
-			host.Verify(h => h.SetPosition(It.IsAny<Vector2>()), Times.Never);
+			var setPositionCount = 0;
+			var deactivateCount = 1;
 
-			// 1.2f is preempt time so at time 0, it should be around the spawning point
-			resource.Setup(r => r.GetPlaybackPosition())
-				.Returns(0);
-			var b = TestingUtils.NewBeatState(timing: 1.2f);
-			pooledBeat.SetActive(b);
+			for (int i = 0; i < 2; i++) // we want to make sure the pooled beat works being reused
+			{
+				pooledBeat.Tick();
+				host.Verify(h => h.Deactivate(), Times.Exactly(deactivateCount));
+				host.Verify(h => h.SetPosition(It.IsAny<Vector2>()), Times.Exactly(setPositionCount));
 
-			pooledBeat.Tick();
-			host.Verify(h => h.SetPosition(It.IsAny<Vector2>()), Times.Once);
-			Assert.Equal(Vector2.One, currentPosition);
+				// 1.2f is preempt time so at time 0, it should be around the spawning point
+				resource.Setup(r => r.GetPlaybackPosition())
+					.Returns(0);
+				var b = TestingUtils.NewBeatState(timing: 1.2f);
+				pooledBeat.SetActive(b);
 
-			// mid point between spawning point and trigger point
-			resource.Setup(r => r.GetPlaybackPosition())
-				.Returns(0.6f);
-			pooledBeat.Tick();
-			host.Verify(h => h.SetPosition(It.IsAny<Vector2>()), Times.Exactly(2));
-			Assert.Equal(Vector2.One * (11f / 2f), currentPosition);
+				pooledBeat.Tick();
+				setPositionCount++;
+				host.Verify(h => h.SetPosition(It.IsAny<Vector2>()), Times.Exactly(setPositionCount));
+				Assert.Equal(Vector2.One, currentPosition);
 
-			resource.Setup(r => r.GetPlaybackPosition())
-				.Returns(1.2f);
-			pooledBeat.Tick();
-			host.Verify(h => h.SetPosition(It.IsAny<Vector2>()), Times.Exactly(3));
-			host.Verify(h => h.Deactivate(), Times.Once);
-			Assert.Equal(Vector2.One * 10f, currentPosition);
+				// mid point between spawning point and trigger point
+				resource.Setup(r => r.GetPlaybackPosition())
+					.Returns(0.6f);
+				pooledBeat.Tick();
+				setPositionCount++;
+				host.Verify(h => h.SetPosition(It.IsAny<Vector2>()), Times.Exactly(setPositionCount));
+				Assert.Equal(Vector2.One * (11f / 2f), currentPosition);
 
-			resource.Setup(r => r.GetPlaybackPosition())
-				.Returns(2.4f);
-			pooledBeat.Tick();
-			host.Verify(h => h.SetPosition(It.IsAny<Vector2>()), Times.Exactly(4));
-			host.Verify(h => h.Deactivate(), Times.Exactly(2));
-			Assert.True(currentPosition.X > 15f);
+				resource.Setup(r => r.GetPlaybackPosition())
+					.Returns(1.2f);
+				pooledBeat.Tick();
+				setPositionCount++;
+				host.Verify(h => h.SetPosition(It.IsAny<Vector2>()), Times.Exactly(setPositionCount));
+				host.Verify(h => h.Deactivate(), Times.Exactly(deactivateCount));
+				Assert.Equal(Vector2.One * 10f, currentPosition);
+
+				resource.Setup(r => r.GetPlaybackPosition())
+					.Returns(2.4f);
+				pooledBeat.Tick();
+				setPositionCount++;
+				host.Verify(h => h.SetPosition(It.IsAny<Vector2>()), Times.Exactly(setPositionCount));
+				deactivateCount++;
+				host.Verify(h => h.Deactivate(), Times.Exactly(deactivateCount));
+				Assert.True(currentPosition.X > 15f);
+			}
 		}
 	}
 }
